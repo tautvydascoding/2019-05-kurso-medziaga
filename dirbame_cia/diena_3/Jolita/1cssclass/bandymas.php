5a0b0f712df07b7334122be0d9ca6cb6bd1f22d0
j.kunciene@gmail.com <j.kunciene@gmail.com>
2019-05-16 09:14:13 +0300

    3d

--- dirbame_cia/diena_3/Jolita/1cssclass/bandymas.php
+++ dirbame_cia/diena_3/Jolita/1cssclass/bandymas.php
@@ -1,3 +1,8 @@
 <?php
+$weight = array("apsauginis1"=>"85", "apsauginis2"=>"67", "apsauginis3"=>"43");
 
+foreach($weight as $x => $x_value) {
+    echo "Key=" . $x . ", Value=" . $x_value;
+    echo "<br>";
+}
 ?>
