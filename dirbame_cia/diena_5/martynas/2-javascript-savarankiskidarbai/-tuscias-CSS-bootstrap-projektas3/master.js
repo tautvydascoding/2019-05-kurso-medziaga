77246d1cd985c10e1d0002ef8190055c28933207
MartynasLa <50550889+MartynasLa@users.noreply.github.com>
2019-05-17 13:27:37 +0300

    5 dienos klases darbai

---
+++ dirbame_cia/diena_5/martynas/2-javascript-savarankiskidarbai/-tuscias-CSS-bootstrap-projektas3/master.js
@@ -0,0 +1,65 @@
+console.log(" Labas   ");
+//  1 UZDUOTIS
+// sukurti kintamuosius (ir jiems priskirti reiksmes):
+// vardas, pavarde, amzius, atlyginimas
+// ir panaudoti:
+// console.log( vardas, pavarde);
+
+let vardas = "Martynas";
+let pavarde = "L";
+let amzius = 3;
+let atlyginimas = 900;
+
+console.log( vardas, pavarde, amzius );
+console.log( atlyginimas );
+
+
+
+
+// teo:
+//  typeof( k ); f-ja kuri isveda kintamojo tipa
+// console.log(    typeof(vardas)  );
+// console.log(    typeof(amzius)  );
+console.log( typeof(vardas) );
+console.log( typeof(amzius) );
+
+
+//  2 UZDUOTIS
+// sukurti funkcija "printVardasPavardeAmzius()" , kuri atspausdina i konsole pirmos uzduoties kintamuosius
+
+function print( ) {
+    console.log( vardas, pavarde, amzius, atlyginimas);
+
+}
+
+print();
+
+//  3 UZDUOTIS
+// sukurti funkcija "printMetinisPajamuDydis()" ,
+// kuri  atspausdina i konsole suma 12 atlyginimu (vienas atlyginimas yra lygus "uzduotis 1" kintamajam - "atlyginimas")
+
+function printMetinisPajamuDydis( ) {
+    let x = 900;
+    let y = 12;
+    let printMetinisPajamuDydis = x * y;
+
+}
+
+//  4 UZDUOTIS
+// A) sukurti kintamuosius:  salis, miestas, adresas, pastoKodas
+// B) sukurti f-ja "printAddressData()",  kuri atspausdina i konsole visus siuos kintamuosius
+
+// ---- advance-----
+//  5 UZDUOTIS
+// sukurti funkcija "printTekstas(x)" ,
+//  kuri atspausdina i konsole "x" reiksmes
+// iskviesti f-ja ir vietoj x irasyti koki nors teksta pvz: printTekstas( "Jokubo istorijos");
+
+
+
+// 7 UZDUOTIS
+// F-ja kuri paskaiciuoja trikampio ilgaja krastine (Pitagoro teorema)
+// pitagoroTeorema(x, y)      x*x + y*y ir is visko istraukti sakti (js Math.sqrt())
+
+//==========================KOMANDINE (3-4h) Bootstrap && GIT=================
+// ant lentos
