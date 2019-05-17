79fefcee6cf19dde6d94f8ba07dd8fc3c65bcffd
valdemaras@litvaitis.com <valdemaras@litvaitis.com>
2019-05-17 16:12:11 +0300

    boot strap pagrindai

---
+++ dirbame_cia/diena_5/valdemaras/1javascriptpagrindai/master.js
@@ -0,0 +1,24 @@
+console.log("Labas");
+
+
+let x = 100;
+let y = 10;
+let z = x + 1;
+console.log(z);
+z = x * y + 2;
+
+let antraste = "viskas prasidejo senai senai...";
+let pabaiga = "ir baigiasi!";
+let istorija = antraste + pabaiga;
+
+console.log(istorija);
+
+function suma() {
+    let x = 1;
+    let y = 3;
+    let z = x + y;
+    console.log(z);
+}
+suma();
+suma();
+suma();
