e57d89aa856087f6c8b78937eef9fa9f4a6bd55e
LennoxEU <lukasmartinaitis1995@gmail.com>
2019-05-19 21:26:47 +0200

    6 dienos namu darbai

---
+++ dirbame_cia/diena_6/Lukas/namu-darbas-50x-funkciju/js/master.js
@@ -0,0 +1,257 @@
+
+// UZDUOTIS =======ismokti f-jas 50x===============
+// Aptrasymas) sukurti 30 getVardas ir 30 setVardas(x) f-ju. Jas iskviesti eiles tvarka "maisant" poruojant pvz:
+// setVardas1("Antanas");
+// getVardas1();
+// setVardas2("Juozas");
+// getVardas2();
+// A) sukurti (gloabalu) kintamaji pvz:
+//      var vardas =  "Tomas";
+// B) sukurti 20-30 f-ju:  getVardas1(), getVardas2(), getVardas3(), ...getVardas30()
+// kurios  turi "return" ir iskvietus f-ja ji  grazina varda ir prideti skaiciu salia  pvz: "Tomas1", Antanas22, Juozas33
+// B) sukurti 20-30 f-ju:   setVardas1(name), setVardas2(name), ... setVardas30(name),...
+// kurios   turi pakeisti varda ir prideti skaiciu salia
+// C) visas f-jas iskviesti
+// pvz.:
+let vardas = "";
+function getVardas1() {
+    console.log(vardas);
+}
+function getVardas2() {
+    console.log(vardas);
+}
+function getVardas3() {
+    console.log(vardas);
+}
+function getVardas4() {
+    console.log(vardas);
+}
+function getVardas5() {
+    console.log(vardas);
+}
+function getVardas6() {
+    console.log(vardas);
+}
+function getVardas7() {
+    console.log(vardas);
+}
+function getVardas8() {
+    console.log(vardas);
+}
+function getVardas9() {
+    console.log(vardas);
+}
+function getVardas10() {
+    console.log(vardas);
+}
+function getVardas11() {
+    console.log(vardas);
+}
+function getVardas12() {
+    console.log(vardas);
+}
+function getVardas13() {
+    console.log(vardas);
+}
+function getVardas14() {
+    console.log(vardas);
+}
+function getVardas15() {
+    console.log(vardas);
+}
+function getVardas16() {
+    console.log(vardas);
+}
+function getVardas17() {
+    console.log(vardas);
+}
+function getVardas18() {
+    console.log(vardas);
+}
+function getVardas19() {
+    console.log(vardas);
+}
+function getVardas20() {
+    console.log(vardas);
+}
+function getVardas21() {
+    console.log(vardas);
+}
+function getVardas22() {
+    console.log(vardas);
+}
+function getVardas23() {
+    console.log(vardas);
+}
+function getVardas24() {
+    console.log(vardas);
+}
+function getVardas25() {
+    console.log(vardas);
+}
+function getVardas26() {
+    console.log(vardas);
+}
+function getVardas27() {
+    console.log(vardas);
+}
+function getVardas28() {
+    console.log(vardas);
+}
+function getVardas29() {
+    console.log(vardas);
+}
+function getVardas30() {
+    console.log(vardas);
+}
+function setVardas1(x) {
+    vardas = x+1;
+}
+function setVardas2(x) {
+    vardas = x+2;
+}
+function setVardas3(x) {
+    vardas = x+3;
+}
+function setVardas4(x) {
+    vardas = x+4;
+}
+function setVardas5(x) {
+    vardas = x+5;
+}
+function setVardas6(x) {
+    vardas = x+6;
+}
+function setVardas7(x) {
+    vardas = x+7;
+}
+function setVardas8(x) {
+    vardas = x+8;
+}
+function setVardas9(x) {
+    vardas = x+9;
+}
+function setVardas10(x) {
+    vardas = x+10;
+}
+function setVardas11(x) {
+    vardas = x+11;
+}
+function setVardas12(x) {
+    vardas = x+12;
+}
+function setVardas13(x) {
+    vardas = x+13;
+}
+function setVardas14(x) {
+    vardas = x+14;
+}
+function setVardas15(x) {
+    vardas = x+15;
+}
+function setVardas16(x) {
+    vardas = x+16;
+}
+function setVardas17(x) {
+    vardas = x+17;
+}
+function setVardas18(x) {
+    vardas = x+18;
+}
+function setVardas19(x) {
+    vardas = x+19;
+}
+function setVardas20(x) {
+    vardas = x+20;
+}
+function setVardas21(x) {
+    vardas = x+21;
+}
+function setVardas22(x) {
+    vardas = x+22;
+}
+function setVardas23(x) {
+    vardas = x+23;
+}
+function setVardas24(x) {
+    vardas = x+24;
+}
+function setVardas25(x) {
+    vardas = x+25;
+}
+function setVardas26(x) {
+    vardas = x+26;
+}
+function setVardas27(x) {
+    vardas = x+27;
+}
+function setVardas28(x) {
+    vardas = x+28;
+}
+function setVardas29(x) {
+    vardas = x+29;
+}
+function setVardas30(x) {
+    vardas = x+30;
+}
+
+setVardas1("Lukas");
+getVardas1();
+setVardas2("antanas");
+getVardas2();
+setVardas3("Petras");
+getVardas3();
+setVardas4("Povilas");
+getVardas4();
+setVardas5("Mykolas");
+getVardas5();
+setVardas6("Monika");
+getVardas6();
+setVardas7("Remigijus");
+getVardas7();
+setVardas8("Jolanta");
+getVardas8();
+setVardas9("Daiva");
+getVardas9();
+setVardas10("Egidijus");
+getVardas10();
+setVardas11("Tomas");
+getVardas11();
+setVardas12("Marius");
+getVardas12();
+setVardas13("Rokas");
+getVardas13();
+setVardas14("Julija");
+getVardas14();
+setVardas15("Gerda");
+getVardas15();
+setVardas16("Kristoforas");
+getVardas16();
+setVardas17("Jonas");
+getVardas17();
+setVardas18("Jokubas");
+getVardas18();
+setVardas19("Janina");
+getVardas19();
+setVardas20("Vaidotas");
+getVardas20();
+setVardas21("Vaidas");
+getVardas21();
+setVardas22("Ausra");
+getVardas22();
+setVardas23("Edita");
+getVardas23();
+setVardas24("Jolita");
+getVardas24();
+setVardas25("Valdemaras");
+getVardas25();
+setVardas26("Martynas");
+getVardas26();
+setVardas27("Ugne");
+getVardas27();
+setVardas28("Liutauras");
+getVardas28();
+setVardas29("Akvile");
+getVardas29();
+setVardas30("Laura");
+getVardas30();
