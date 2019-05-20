9e7ec80e4bc4f30730830438b226c86905b56298
srn.zukauskas@gmail.com <srn.zukauskas@gmail.com>
2019-05-20 15:08:07 +0300

    uzduoties keitimas
    nd

--- dirbame_cia/diena_6/Sarunas/1-js/main.js
+++ dirbame_cia/diena_6/Sarunas/1-js/main.js
@@ -1,4 +1,5 @@
 console.log(" Labas ");
+
 //=====================PASIKARTOJIMAS==================
 
 //  1.1 UZDUOTIS
@@ -6,11 +7,11 @@ console.log(" Labas ");
 //  kuri atspausdina i konsole "x" reiksme
 //  iskviesti f-ja print(15.99);
 
-
 function printKaina(x) {
     console.log( x );
 }
 printKaina( 15.99 );
+
 
 //  1.2 UZDUOTIS
 // iskviesti f-ja print 3 kartus, kad atspausdintu 3 skirtingas kainas:  999 , 13.49, 100.05
@@ -19,18 +20,17 @@ printKaina( 25.99 );
 printKaina( 35.99 );
 printKaina( 110.99 );
 
-
 // 2.1 UZDUOTIS
 // parasyti f-ja "pazymiuVidurkis()", kuri apskaiciuoja vidurki is  5 menesiu pazymiu
 // suskaiciuoti pazymiu vidurki (pazymius sukurti f-jos viduje) 5, 10 , 8 , 6 , 8  (ir atspausdintu vidurki)
 function pazymiuVidurkis() {
-  let x1 = 10;
-  let x2 = 10;
-  let x3 = 10;
-  let x4 = 10;
-  let x5 = 5;
-  let vidurkis = ((((x1 + x2 + x3 + x4 + x5))) / 5);
-  document.write("vidurkis yra:" + vidurkis);
+    let x1 = 10;
+    let x2 = 10;
+    let x3 = 10;
+    let x4 = 10;
+    let x5 = 5;
+    let vidurkis = ((((x1 + x2 + x3 + x4 + x5))) / 5);
+    document.write(" vidurkis yra:"  + vidurkis);
 }
 pazymiuVidurkis();
 pazymiuVidurkis();
@@ -41,39 +41,62 @@ pazymiuVidurkis();
 // kuri apskaiciuoja vidurki is paduotu 5 menesiu pazymiu
 // suskaiciuoti pazymiu vidurki, 5, 10 , 8 , 6 , 8  (ir atspausdintu vidurki)
 
-function getPazymiuVidurkis2(x1, zzz, x3, x4, x5) {
-    let ats = (x1 + zzz + x3 + x4 + x5) / 5;
-    console.log( vardas + " " + "Vidurkis " + ats);
+function pazymiuVidurkis1(x1, zzz, x3, x4, x5) {
+    let ats = (x1 + zzz  + x3 + x4 + x5) / 5;
+    console.log( "Vidurkis " + ats );
 }
-pazyniuVidurkis( 10, 10, 10, 10, 10);
-pazyniuVidurkis( 2, 2, 2, 5, 6);
-pazyniuVidurkis( 4, 5, 4, 6, 7);
-
- pazymiuVidurkis2("Ieva", 10, 9, 10, 5, 5);
- pazymiuVidurkis2("Povilas", 4, 5, 6, 7, 7);
- pazymiuVidurkis2("Marius", 4, 4, 4, 3, 5);
-
-
-
-
-
- function suma( x1, x2){
-   let ats = x1 + x2;
-   return ats; //grazinni i iskvietimo vieta salia parasyta reiksme
-
- }
-suma(10, 2);
+pazymiuVidurkis1(  10, 10, 10, 10, 10);
+pazymiuVidurkis1(  2, 2, 2, 5, 6);
+pazymiuVidurkis1(  4, 5, 4, 6, 7);
+// ------
+function pazymiuVidurkis2(vardas ,x1, zzz, x3, x4, x5) {
+    let ats = (x1 + zzz  + x3 + x4 + x5) / 5;
+    console.log( vardas + " " + "Vidurkis " + ats );
+}
+pazymiuVidurkis2("Ieva",   10, 9, 10, 5, 5);
+pazymiuVidurkis2("Povilas", 4, 5, 6, 7, 7);
+pazymiuVidurkis2("Marius",  4, 4, 4, 3, 5);
 //=====================RETURN==================
+function suma(   x1, x2) {
+    let ats =  x1 + x2;
+    return ats;  // grazini i iskvieitmo vieta salia parasyta reiksme
+}
+let galutineSuma = suma(10, 2);
+console.log(  galutineSuma   );
+
+// arba
+console.log(   suma(10, 2)     );
+
+
+//=====================RETURN==================
+// function suma(   ) {
+//     let ats =  10 + 20;
+// }
+// console.log( "suma yra:" +  ats );
+// let x = 10;
 
 // 1 UZDUOTIS
 // A) sukurti kintamaji "vardas" Tomas
 // B) parasyti f-ja "getVardas()", kuri turi "return" zodeli ir grazina varda i iskveitimo vieta.
 // C) patikrinti ar veikia f-ja
+function getVardas() {
+    let vardas = "Tomas";
+    return vardas
+}
+console.log( getVardas()  );
 
 // 2 UZDUOTIS
 // parasyti f-ja "getVardasPavarde()", kuri turi "return" zodeli ir grazina varda ir pavarde i iskveitimo vieta.
 //  f-joje sukurti kintamaji   "pavarde" Tomauskas, "vardas" - Antanas
 // patikrinti ar veikia f-ja
+// function getVardasPavarde() {
+//     let pavarde = "Tomauskis";
+//     let vardas = "Jonas";
+//     let getVardasPavarde= vardas + " " + pavarde;
+//     return getVardasPavarde
+// }
+// console.log( getVardasPavarde();
+// console.log(  );
 
 // 3 UZDUOTIS
 // A) parasyti f-ja "getPelnas()", kuri turi "return" zodeli ir grazina apskaiciuota pelna
@@ -81,6 +104,24 @@ suma(10, 2);
 // B) f-joje sukurti kintamaji "islaidos" 7500
 // B) f-joje apskaiciuoti pelna  ( pvz: pelnas = pajamos - islaidos)
 // C) patikrinti ar veikia f-ja
+
+// 3.1) parasyti f-ja "grynasPelnas()",
+// kuri turi kintamaji pelno "moketis" = 10%
+// apskaiciuoti gryna pelna (is pelno atemus pelno mokesti)
+
+function getPelnas() {
+    let pajamos = 12500;
+    let islaidos = 7500;
+    return apmokestpelnas = pajamos - islaidos;
+}
+console.log(getPelnas());
+
+function getGrynas() {
+    let mokestis = 0.1;
+    return getPelnas() - (getPelnas()*mokestis);
+}
+console.log(getGrynas());
+
 
 // 3.2 UZDUOTIS
 // A) parasyti f-ja "getPelnas(pajamos, nuostoliai)", kuri turi "return" zodeli ir grazina apskaiciuota pelna
@@ -92,7 +133,7 @@ suma(10, 2);
 // parasyti f-ja, kuri turi "return" zodeli.
 // "getPazymiuVidurkis2(x1, x2, x3, x4, x5)"  f-ja apskaiciuoja vidurki is paduotu 5 menesiu pazymiu (ir grazina atsakyma i iskvietimo vieta! "return atskymas;")
 
-
+// parasyti f-ja, kuri skaiciuoja vieno mokinio pazymiu vidurki
 
 // 5 UZDUOTIS
 // A) sukurti h1 su tekstu viduje  (HTML faile)
