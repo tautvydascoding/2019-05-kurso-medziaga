c8c65999ccd9cb08680a188abd8819ab9f1760ee
srn.zukauskas@gmail.com <srn.zukauskas@gmail.com>
2019-05-21 16:04:20 +0300

    sarunas martynas

---
+++ dirbame_cia/diena_7/Sarunas/1-for-ciklas/js/main.js
@@ -0,0 +1,20 @@
+    console.log(" Labas ");
+
+    let i = 0;
+
+    // 10 kartu atspausdinti zodi "Labas"
+    // 10 - kiek kartu ciklas veiks
+    for ( i = 0;  i < 10; i++) {
+      document.write( "Labas <br> ");
+    }
+    for ( i = 0; i < 8; i++) {
+      document.write( i + "<br>");
+      
+    }
+    //nuo kito galo nuo 10 maziname iki 0
+    // for ( i = 10; i > 0; i--) {
+    //   document.write( i + "<br>");
+    // }
+    //
+
+//
