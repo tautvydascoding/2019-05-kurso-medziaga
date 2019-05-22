e52f8b522bb0c4c7cc85c1ae4a930a376fa90a36
Lukas <lukasmartinaitis1995@gmail.com>
2019-05-22 16:32:01 +0300

    uzdaviniai  su masyvais

--- dirbame_cia/diena_8/Lukas/uzdt1array/js/main.js
+++ dirbame_cia/diena_8/Lukas/uzdt1array/js/main.js
@@ -14,5 +14,39 @@
 
 	// 3.1  padaryti 3) uzdaviniui f-ja sukeistiMasyvo2elementus(x, y)
 
-    var prekiautojai = ["senukai", "maxima", "iki", "rimi", "norfa"];
+    let prekiautojai = ["senukai", "maxima", "iki", "rimi", "norfa"];
     console.log(prekiautojai);
+    prekiautojai[0] = prekiautojai[0] + " ir KO";
+    console.log(prekiautojai);
+    let temp = prekiautojai[2];
+    prekiautojai[2] = prekiautojai[1];
+    prekiautojai[1] = temp;
+    console.log(prekiautojai);
+
+    function sukeistiMasyvo2elementus(x, y) {
+        let laikinas = prekiautojai[y];
+        prekiautojai[y] = prekiautojai[x];
+        prekiautojai[x] = laikinas;
+        console.log(prekiautojai);
+    }
+
+    sukeistiMasyvo2elementus(1, 2);
+    
+//     function move(arr, old_index, new_index) {
+//     while (old_index < 0) {
+//         old_index += arr.length;
+//     }
+//     while (new_index < 0) {
+//         new_index += arr.length;
+//     }
+//     if (new_index >= arr.length) {
+//         var k = new_index - arr.length;
+//         while ((k--) + 1) {
+//             arr.push(undefined);
+//         }
+//     }
+//      arr.splice(new_index, 0, arr.splice(old_index, 1)[0]);
+//    return arr;
+// }
+//     move(prekiautojai, 1, 2);
+//     console.log(prekiautojai);
