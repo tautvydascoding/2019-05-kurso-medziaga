ec16caf167151efa03a0e505f84b276677712c19
ugnegaja <50557168+ugnegaja@users.noreply.github.com>
2019-05-24 12:28:06 +0300

    Atlikta paieskos uzduotis

--- dirbame_cia/diena_10/Ugne/2 - paieska/js/main.js
+++ dirbame_cia/diena_10/Ugne/2 - paieska/js/main.js
@@ -1,1 +1,74 @@
 console.log(" Labas ");
+let i = 0;
+
+// =================taisyklingas Array copy  =================
+var array = ['a', 'b', 'c'];  // Becomes arr1 = ['a', 'b', 'c']
+var kopijaNEGERAI =  array; // !!! Blogi  - sukurs susietaja kopija ir redaguojant keisis abu masyvai
+var kopija1 = array.slice(0);   // Becomes arr2a = ['a', 'b', 'c'] - deep copy
+var kopija2 = array.concat(  );   // Becomes arr2b = ['a', 'b', 'c'] - deep copy
+
+// ================= ========= ======== ========
+ ieskomiZmones = [2, 16, 17, 18, 19, 25];
+
+var names = ["Enriqueta", "Sybil", "Piper", "Anh", "Carmelo", "Regan", "Synthia", "Newton", "Norbert", "Krystyna", "Fidelia", "Christoper", "Lewis", "Jeromy", "Joy", "Lorri", "Owen", "Rosenda", "Devora", "Treva", "Leanora", "Meghann", "Jacqueline", "Bunny", "Tenisha", "Rico", "Clementina", "Samella", "Rico", "Sandi", "Efrain", "Tena", "Vivan", "Hiedi", "Naida", "Evette", "Shane", "Freida", "Marielle", "Wynona", "Cheree", "Gaston", "Aja", "Timika", "Jude", "Griselda", "Luise", "Rico", "Minh", "Warren"];
+
+var lastNames = ["Mcdowell", "Gates", "Mccall", "Cisneros", "Hancock", "Gaines", "Juarez", "Nolan", "Barajas", "Ware", "Orr", "Bell", "Donovan", "Rojas", "Stevenson", "Long", "Hays", "Gibson", "Meyer", "Sims", "Mcintosh", "Craig", "Haney", "Cunningham", "Hunt", "Montgomery", "Spears", "Cooke", "Gregory", "Mcknight", "Fernandez", "Hendrix", "Patton", "Bond", "Skinner", "Randolph", "Montes", "Guerra", "Bowen", "Potts", "Dyer", "Riley", "Rodgers", "Schroeder", "Ferguson", "Garrett", "Rush", "Moon", "Whitney", "Mcdaniel"];
+
+
+
+// 1A) surasti vardu masyve, kelintas zmogus yra "Rico" (surasti pirmaji; sunkes- surasti visus riko)
+
+// 1B) papildyti ^: jeigu tokio vardo neranda, isvesti VIENĄ pranesima "Nepavyko rasti...Bandykite kita zodi"
+
+function paieska(vardas){
+ var nerodo = 0;
+  for (i=0; i<names.length; i++){
+  if (names[i]===vardas){
+    console.log(vardas + " numeris masyve yra: " + i)
+  } else{
+    nerodo++;
+  }
+}
+
+if (nerodo == names.length){
+  console.log("Nepavyko rasti...Bandykite kita zodi");
+}
+}
+
+paieska("Rico");
+
+
+
+// 2 UZDUOTIS (f-ja iekom stalciaus)
+// parasyti funkcija, kuriai davus iekoma zodi , ji suranda jo vieta masyve (stalciaus numeri) ir si   numeri grazina
+// eg: getStalciausNumeris( ieskomasTekstas)
+
+// uzduotis:----------------------------
+// 3) rasti pavarde masyve esancio  zmogaus vardu "Freida" (pirmojo) DONE
+
+function paieskaPavarde(vardas){
+  for (i=0; i<names.length; i++){
+  if (names[i]===vardas){
+    console.log(vardas + " pavarde yra: " + lastNames[i])
+  }
+}
+}
+
+paieskaPavarde("Freida");
+paieskaPavarde("Rico");
+
+// 4) rasti visu zmoniu vardu "Rico" pavardes DONE
+// 5) Turime masyva su zmoniu nr.  ieskomiZmones = [2, 16, 17, 18, 19, 25];
+// A) atspausdinti visus numerius
+// B) isvesti ju pavardes ir vardus
+
+for (i=0; i<ieskomiZmones.length; i++ ){
+let x = ieskomiZmones[i];
+console.log("Numeris " + x + " yra: " + names[x] + " " + lastNames[x])
+}
+
+
+// var a = Math.random(); // 0 - 1
+// if ( a <= 0.1) {
+//     console.log("laimejai");
+// }
