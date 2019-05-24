9aa3986e6312d1a35825ac45114007a4e02299af
MartynasLa <50550889+MartynasLa@users.noreply.github.com>
2019-05-24 16:35:47 +0300

    10 dienos popeitiniai darbai

--- dirbame_cia/diena_10/martynas/2-javascript-array4.1/-tuscias-bootstrap-projectas-7/js/main.js
+++ dirbame_cia/diena_10/martynas/2-javascript-array4.1/-tuscias-bootstrap-projectas-7/js/main.js
@@ -9,7 +9,7 @@ var kopija1 = array.slice(0);   // Becomes arr2a = ['a', 'b', 'c'] - deep copy
 var kopija2 = array.concat(  );   // Becomes arr2b = ['a', 'b', 'c'] - deep copy
 
 // ================= ========= ======== ========
- ieskomiZmones = [2, 16, 17, 18, 19, 25];
+
 
 var names = ["Enriqueta", "Sybil", "Piper", "Anh", "Carmelo", "Regan", "Synthia", "Newton", "Norbert", "Krystyna", "Fidelia", "Christoper", "Lewis", "Jeromy", "Joy", "Lorri", "Owen", "Rosenda", "Devora", "Treva", "Leanora", "Meghann", "Jacqueline", "Bunny", "Tenisha", "Rico", "Clementina", "Samella", "Rico", "Sandi", "Efrain", "Tena", "Vivan", "Hiedi", "Naida", "Evette", "Shane", "Freida", "Marielle", "Wynona", "Cheree", "Gaston", "Aja", "Timika", "Jude", "Griselda", "Luise", "Rico", "Minh", "Warren"];
 
@@ -18,17 +18,25 @@ var lastNames = ["Mcdowell", "Gates", "Mccall", "Cisneros", "Hancock", "Gaines",
 
 
 // 1A) surasti vardu masyve, kelintas zmogus yra "Rico" (surasti pirmaji; sunkes- surasti visus riko)
-let ieskomasTekstas="Rico";
-let arRadau=false;
-for (i = 0; i < 50; i++) {
-    if (names[i]===ieskomasTekstas) {
-        console.log(i + "Radau Rico" + ieskomasTekstas);
-        arRadau = true;
-    }
-}
- if (arRadau===false) {
-     console.log("Nepavyko rasti...Bandykite kita zodi")
- }
+// let ieskomasTekstas="Rico";
+// let arRadau=false;
+// for (i = 0; i < 50; i++) {
+//     if (names[i]===ieskomasTekstas) {
+//         console.log(i + "Radau Rico" + ieskomasTekstas);
+//         arRadau = true;
+//     }
+// }
+//  if (arRadau===false) {
+//      console.log("Nepavyko rasti...Bandykite kita zodi")
+//  }
+
+//  for ( i = 0; i < 3; i++){
+//      console.log("A");
+//      for ( t = 0; i < 99; i++) {
+//          console.log("B");
+//          break;
+//  }
+
 
 
 // 1B) papildyti ^: jeigu tokio vardo neranda, isvesti VIENĄ pranesima "Nepavyko rasti...Bandykite kita zodi"
@@ -37,19 +45,73 @@ for (i = 0; i < 50; i++) {
 // 2 UZDUOTIS (f-ja iekom stalciaus)
 // parasyti funkcija, kuriai davus iekoma zodi , ji suranda jo vieta masyve (stalciaus numeri) ir si   numeri grazina
 // eg: getStalciausNumeris( ieskomasTekstas)
-function ieskomStalciaus() {
 
 
 
+// function getStalciausNumeris( ieskomas) {
+//     for (i = 0; i < names.length; i++) {
+//         if (names[i] == ieskomas) {
+//             return i;
+//
+//         }
+//     }
+// }
+//
+// let riconumneris = getStalciausNumeris("Rico");
+// let pipernumeris = getStalciausNumeris("Piper");
+// console.log("Rico numeris " + riconumneris);
+// console.log("Piper numeris " + pipernumeris);
 
-}
-
-ieskomStalciaus();
 // uzduotis:----------------------------
 // 3) rasti pavarde masyve esancio  zmogaus vardu "Freida" (pirmojo)
+// let ieskomas = "Freida"
+
+
+// for (i = 0; i < names.length; i++) {
+//     if (names[i] == ieskomas) {
+//         ieskomasNr = i;
+//         break;
+//     }
+// }
+//
+// console.log( "pavarde" + lastNames[i] );
+
 // 4) rasti visu zmoniu vardu "Rico" pavardes
-// 5) Turime masyva su zmoniu nr.  ieskomiZmones = [2, 16, 17, 18, 19, 25];
+let ieskomasNr = -999;
+let ieskomas = "Rico";
+
+for (i = 0; i < names.length; i++) {
+    //tikrinu ar masyvo stalciu yra mano tekstas
+    if (names[i] == ieskomas) {
+        //radau Rico
+        ieskomasNr = i;
+        console.log(ieskomas + " " + lastNames[ieskomasNr]);
+    }
+}
+
+
+
+
+
+// 5) Turime masyva su zmoniu nr.  ieskomiZmones = [2, 16, 17, 18, 19, 25]
 // A) atspausdinti visus numerius
+let ieskomiZmones = [2, 16, 17, 18, 19, 25];
+
+for ( i = 0; i < ieskomiZmones.length; i++) {
+    console.log(ieskomiZmones[i]);
+
+    console.log( names[ieskomiZmones[i]] + " " + lastNames[ieskomiZmones[i]]);
+}
+
+
+
+// for (i = 0; i < ieskomiZmones.length; i++) {
+//     if (ieskomiZmones[i]==lastNames) {
+//         ieskomasNr = i;
+//         console.log();
+//     }
+// }
+
 // B) isvesti ju pavardes ir vardus
 
 
