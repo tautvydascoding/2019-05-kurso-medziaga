54f8d082184576dd4e41e39e4d29e306738c4b59
ugnegaja <50557168+ugnegaja@users.noreply.github.com>
2019-05-24 10:26:54 +0300

    paieska uzduotis

--- dirbame_cia/diena_10/Ugne/1 - debugger/js/main.js
+++ dirbame_cia/diena_10/Ugne/1 - debugger/js/main.js
@@ -1,1 +1,11 @@
 console.log(" Labas ");
+let i = 0;
+//Kas treciam duoti paskola, turim 12 norinciu
+let arDuotiPaskola = false;
+let pretendentai = [];
+
+for ( i = 0; i < 13; i+=3){
+  pretendentai[i] = true;
+}
+
+console.log(pretendentai);
