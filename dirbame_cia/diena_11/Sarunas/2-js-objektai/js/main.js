2b0c8d0e2e4605e5b272281bdfdac506854b1a6b
srn.zukauskas@gmail.com <srn.zukauskas@gmail.com>
2019-05-28 09:12:04 +0300

    a

--- dirbame_cia/diena_11/Sarunas/2-js-objektai/js/main.js
+++ dirbame_cia/diena_11/Sarunas/2-js-objektai/js/main.js
@@ -1,20 +1,2 @@
 console.log(" Labas ");
-let vardas = "TOmas"
-
-
-$('h1').hide(3000);
-
-$("button").click(function(){
-  $("p").hide()
-});
-$("rodyti").click(function(){
-  $("p").show(1500); //1.5sek
-  });
-  //isvesti i console mygtuko HTML elementa
-  console.log( document.queryselector(".paslepti" ) );
-  //arba
-  let manoMygtukas = document.queryselector(".paslepti");
-  let manoMygtukas2 = $(".paslepti");
-  console.log( manoMygtukas.innerHTML);
-  console.log( manoMygtukas );
-  console.log( manoMygtukas );
+let vardas = "TOmas";
