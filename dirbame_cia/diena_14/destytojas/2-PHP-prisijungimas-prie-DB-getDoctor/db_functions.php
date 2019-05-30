018ec5beff056d8b4ed39dbb63aba2506fde7afa
tautvydascoding@gmail.com <Tautvydascoding@gmail.com>
2019-05-30 15:16:05 +0300

    prisijungimas

---
+++ dirbame_cia/diena_14/destytojas/2-PHP-prisijungimas-prie-DB-getDoctor/db_functions.php
@@ -0,0 +1,16 @@
+<?php
+
+    require_once('nustatymai.php');
+    // include_once('nustatymai.php');
+    // include('nustatymai.php');
+
+    //PRISIJUNTIMAS
+    $prisijungimas = mysqli_connect(DB_HOST,  MYSQL_USER, MYSQL_PASSWORD, DB_NAME   );
+
+    if( $prisijungimas ) {
+        echo "Prisijungete sekmingai! <br />";
+    } else {
+        echo "ERROR:  prisijungti prie DB nepavyko  !!!" . mysqli_connect_error();
+    }
+
+ ?>
