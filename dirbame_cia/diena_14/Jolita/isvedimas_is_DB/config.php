cd6a34b0449a810f2108df1b429a696b696df0fa
j.kunciene@gmail.com <j.kunciene@gmail.com>
2019-06-02 22:55:10 +0300

    54min15pask.2d

---
+++ dirbame_cia/diena_14/Jolita/isvedimas_is_DB/config.php
@@ -0,0 +1,13 @@
+<?php
+//kuriame constantas
+
+define('DB_NAME', 'hospital5');
+define('MYSQL_USER', 'root');
+define('MYSQL_PASSWORD', '');
+define('DB_HOST', 'localhost');
+
+define('HOME_NUMBER_OF_ARTICLES', 8 );
+define('RESULT_NUMBER_OF_ARTICLES', 12 );
+
+
+ ?>
