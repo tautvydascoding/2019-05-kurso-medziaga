75d8db182f51449c96b4c13910ad0f9ce22cdae2
tautvydascoding@gmail.com <Tautvydascoding@gmail.com>
2019-06-03 12:34:43 +0300

    update DB

--- dirbame_cia/diena_16/destytojas/1-php-CRUD-dygytoju-f-jos/db_functions.php
+++ dirbame_cia/diena_16/destytojas/1-php-CRUD-dygytoju-f-jos/db_functions.php
@@ -39,6 +39,57 @@
     // $gydytojas1 = getDoctor( 1 );
     // $gydytojas4 = getDoctor( 4 );
 
-    //
+    // 5. createDoctor($vardas, $pavarde)
+function createDoctor( $vardas, $pavarde) {
+    $vardasUzkoduotas = mysqli_real_escape_string (getPrisijungtimas(), $vardas );
+    $pavardeUzkoduotas = mysqli_real_escape_string (getPrisijungtimas(), $pavarde );
+
+    $manoSQL = "INSERT INTO  doctors
+                    VALUES( null, '$vardasUzkoduotas', '$pavardeUzkoduotas') ";
+    $rezultatai = mysqli_query(getPrisijungtimas(),  $manoSQL); // print_r(    $rezultataiOBJ );  // test
+    if ( !$rezultatai) {
+        echo "ERROR: nepavyko sukurti gydytojo. SQL klaida:" . mysqli_error(getPrisijungtimas());
+    } else {
+        // echo "Gydytojas uzregistruotas!";
+    }
+}
+// createDoctor("Gedas", "Karlova");
+// createDoctor("Karolis", "Kukutis");
+// createDoctor("Lina", "Lietuvaite");
+    // 6. deleteDoctor($nr)
+function deleteDoctor($nr) {
+    $manoSQL = "DELETE FROM doctors WHERE id=$nr LIMIT 1";
+
+    $rezultatai = mysqli_query(getPrisijungtimas(),  $manoSQL); // print_r(    $rezultataiOBJ );  // test
+    if ( !$rezultatai) {
+        echo "ERROR: nepavyko istrinti gydytojo. SQL klaida:" . mysqli_error(getPrisijungtimas());
+    } else {
+        // echo "Gydytojas uzregistruotas!";
+    }
+}
+deleteDoctor(8);
+    // 7. updateDoctor($nr, $vardas, $pavarde)
+function    updateDoctor($nr, $vardas, $pavarde) {
+    $nrUzkoduotas = mysqli_real_escape_string (getPrisijungtimas(), $nr );
+    $vardasUzkoduotas = mysqli_real_escape_string (getPrisijungtimas(), $vardas );
+    $pavardeUzkoduotas = mysqli_real_escape_string (getPrisijungtimas(), $pavarde );
+
+
+    $manoSQL = "UPDATE doctors SET
+                                    name = '$vardasUzkoduotas',
+                                    lname = '$pavardeUzkoduotas'
+                                WHERE id = '$nrUzkoduotas'
+                                LIMIT 1
+              ";
+    $rezultatai = mysqli_query(getPrisijungtimas(),  $manoSQL); // print_r(    $rezultataiOBJ );  // test
+    if ( !$rezultatai) {
+        echo "ERROR: nepavyko redaguoti. SQL klaida:" . mysqli_error(getPrisijungtimas());
+    } else {
+        // echo "Gydytojas uzregistruotas!";
+    }
+}
+updateDoctor(6, "Jonas", "Jonaitis");
+
+    // 8. getDoctors($kiekGydytoju=99999)
 
  ?>
