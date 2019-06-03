ad3e2409e90205ae9b35723a64cb1829792e6f96
srn.zukauskas@gmail.com <srn.zukauskas@gmail.com>
2019-06-03 13:37:56 +0300

    siunciuos

--- dirbame_cia/diena_16/Sarunas/db_functions.php
+++ dirbame_cia/diena_16/Sarunas/db_functions.php
@@ -7,9 +7,6 @@
     //PRISIJUNTIMAS
     $prisijungimas = mysqli_connect(DB_HOST,  MYSQL_USER, MYSQL_PASSWORD, DB_NAME   );
 
-    // kad lietuviskos ir ru veiktu
-    mysqli_set_charset($prisijungimas, 'utf8mb4');
-    
     // ! - AR FALSE?
     if( !$prisijungimas )   {
         echo "ERROR:  prisijungti prie DB nepavyko  !!!" . mysqli_connect_error();
@@ -24,23 +21,66 @@
     $nr - id duomenu bazeje (gydytojo)
     return - array
 */
-    function getDoctor( $nr ) {
+    function getNav( $nr ) {
         // $query
-        $manoSQL = "SELECT * FROM doctors  WHERE id='$nr'   ";
+        $manoSQL = "SELECT * FROM navigacija  WHERE id='$nr'   ";
         // $rezultatai - mysql'o objektas
         $rezultataiOBJ = mysqli_query(getPrisijungtimas(),  $manoSQL); // print_r(    $rezultataiOBJ );  // test
         $rezultataiArray = mysqli_fetch_assoc($rezultataiOBJ);     // print_r(    $rezultataiArray );  // test
         // $rezultataiArray = mysqli_fetch_row($rezultataiOBJ);     // print_r(    $rezultataiArray );  // test
         return $rezultataiArray;
     }
-
+    // print_r(getNav(1));
     // $gydytojas2 = getDoctor( 2 );
     // print_r($gydytojas2); // test
     // $gydytojas3 = getDoctor( 3 );
     // print_r($gydytojas3); // test
     // $gydytojas1 = getDoctor( 1 );
-    // $gydytojas4 = getDoctor( 4 );
 
+    // $gydytojas4 = getDoctor( 4 );
+    //5. createDoctor($vardas, $pavarde)
+    // function createDoctor( $vardas, $pavarde) {
+    //   $vardasUzkoduotas = mysqli_real_escape_string (getPrisijungtimas())
+    //   $manoSQL = "INSERT INTO doctors
+    //                 VALUES( null, '$vardas', '$pavarde' ) ";
+    //   $rezultatai = mysqli_query(getPrisijungtimas(), $manoSQL); //$prisijungimas
+    //   if (!$rezultatai) {
+    //       echo "ERROR: nepavyko sukurti gydytojo. SQL klaida:" . mysqli_error(getPrisijungtimas());
+    //   }
+    //
+    // }
+    // createDoctor("Karolina", "Karlova");
+    // createDoctor("Karolis", "Kukutis");
+    // createDoctor("Lina", "Lietuvaite");
+
+    // 6. deleteDoctor($nr)
+    function deleteDoctor($nr) {
+        manoSQL = "DELETE FROM DOCTORS WHERE ID =$nr";
+
+        $rezultatai = mysqli_query(getPrisijungtimas(), $manoSQL);
+        if ( !$rezultatai) {
+          echo "ERROR: nepavyko istrinti gydytojo. SQL klaida:" . mysqli_error(getPrisijungtimas());
+        } else {
+
+        }
+
+    }
+    deleteDoctor(4);
+    // 7. updateDoctor($nr, $vardas, $pavarde)
+  function updateDoctor($nr, $vardas, $pavarde) {
+    $manoSQL = "UPDATE doctors SET"
+                                name = '$vardas',
+                                lname = '$pavarde'
+                              WHERE id = '6'
+                              LIMIT 1
+                      "  ;"
+
+                        $rezultatai = mysqli_query(getPrisijungtimas(), $manoSQL);
+                        if ( !$rezultatai) {
+                          echo "ERROR: nepavyko istrinti gydytojo. SQL klaida:" . mysqli_error(getPrisijungtimas());
+                        } else {
+  }
+    // 8. getDoctors($kiekGydytoju=99999)
     //
 
  ?>
