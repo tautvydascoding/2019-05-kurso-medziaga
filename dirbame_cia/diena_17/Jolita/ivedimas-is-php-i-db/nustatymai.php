7ae573ef35e4431a230e4e829436efed209ecee1
j.kunciene@gmail.com <j.kunciene@gmail.com>
2019-06-04 10:20:20 +0300

    16d

--- dirbame_cia/diena_17/Jolita/ivedimas-is-php-i-db/nustatymai.php
+++ dirbame_cia/diena_17/Jolita/ivedimas-is-php-i-db/nustatymai.php
@@ -2,7 +2,7 @@
 // konstantos (nekintantys kintamieji)
 define('DB_NAME', 'hospital5' );
 define('MYSQL_USER', 'root' );
-define('MYSQL_PASSWORD', '' );
+define('MYSQL_PASSWORD', 'root' );
 define('DB_HOST', 'localhost' );
 
 define('HOME_NUMBER_OF_ARTICLES', 8 );
