5a93a42414ab6943e4019d9e7089c172cf221db0
valdemaras@litvaitis.com <valdemaras@litvaitis.com>
2019-06-07 23:20:11 +0300

    20 diena

---
+++ dirbame_cia/diena_20/valdemaras/2-PHP-paveldimumas/gyvunas.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ *
+ */
+
+class Gyvunas
+{
+    public $svoris = "150";
+    private $pusryciai = "barsukas";
+    protected $ligos = "cukralige";
+    public function printPusryciai(){
+        echo $this->pusryciai;
+    }
+}
