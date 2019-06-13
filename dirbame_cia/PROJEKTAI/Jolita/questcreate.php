21d18b8e959cb832f1c1f4ba124335420589c4d3
j.kunciene@gmail.com <j.kunciene@gmail.com>
2019-06-13 15:25:36 +0300

    projektas

--- dirbame_cia/PROJEKTAI/Jolita/questcreate.php
+++ dirbame_cia/PROJEKTAI/Jolita/questcreate.php
@@ -2,4 +2,5 @@
 include ('db_functions.php');
 createQuestion($_GET['name'], $_GET['email'], $_GET['subject']);
    ?>
+  
  <a  href="adminmeniu.php" class="btn btn-outline-dark w-50 mb-5">Back to admin panel</a>
